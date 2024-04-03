327155c504a9413cdf8290cffe6681fc462f4c64
Kaveh Hormoz Manesh <reza2boyce@gmail.com>
2024-04-03 23:13:59 +0330

    indent blankline plugin added

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -119,6 +119,11 @@ _G.packer_plugins = {
     path = "/home/womoboy/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
+  ["indent-blankline.nvim"] = {
+    loaded = true,
+    path = "/home/womoboy/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    url = "https://github.com/lukas-reineke/indent-blankline.nvim"
+  },
   ["lspkind.nvim"] = {
     loaded = true,
     path = "/home/womoboy/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
