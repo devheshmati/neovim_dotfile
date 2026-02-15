9bed9cef84ecdff3cd61fe2769c7fc39778516b3
devheshmati <reza2boyce@gmail.com>
2026-02-15 16:37:01 -0500

    update indentaion and tab key space indentation

--- lua/config/option.lua
+++ lua/config/option.lua
@@ -8,3 +8,19 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+
+-- this is for educational for custiomize tab indentation space for special file type, in this example for HTML
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "html",
+--   callback = function()
+--     vim.opt_local.tabstop     = 2
+--     vim.opt_local.shiftwidth  = 2
+--     vim.opt_local.expandtab   = true
+--   end,
+-- })
